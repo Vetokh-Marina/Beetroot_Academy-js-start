fb953e8a4f77176e2eb5aa4cbfd76ce28448f7b3
Vetokh-Marina <marina.vetoh@gmail.com>
2023-07-01 02:02:33 +0300

    add script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+1. Створити 5 змінних: 
+    1) Назва нашої академії
+    2) Назва нашого курсу
+    3) Назва нашої країни
+    4) Ім'я студента
+    5) Зарплата розробника 
+2. Підключити до html файлу скрипт
+*/
+let academyName = 'Beetroot-Academy',
+   courseName = 'Front-end',
+   country = 'Ukraine',
+   studentName = 'Vetokh Maryna',
+   salary = '10 000$';
+console.log(academyName, courseName, country, studentName, salary);
